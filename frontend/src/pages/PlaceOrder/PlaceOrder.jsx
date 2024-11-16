1ae68c63b0b4f99d8a41a10687a1006123722faa
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-11-16 12:35:33 +0530

    complete

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 
 const PlaceOrder = () => {
@@ -60,7 +61,16 @@ const PlaceOrder = () => {
       }
   
     }
-
+    const navigate=useNavigate();
+    useEffect(()=>{
+      if (!token) {
+           navigate('/cart')
+      } else if (getTotalCartAmount()===0) {
+       navigate('/cart')
+      } 
+       
+      
+   },[token])
 
 
 
